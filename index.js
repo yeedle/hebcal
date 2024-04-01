9502faeb4a4fcd01777fe1d1450f7a1eb16f5d7e
Abraham Neuwirth <abeneuwirth@gmail.com>
2024-03-31 23:39:23 -0400

    refactored index->zmanim into function

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const newYork = Location.lookup("New York");
 const today = new HDate();
 const sunday = today.onOrBefore(0);
 
-const daysOfTheWeek = [0, 1, 2, 3, 4, 5, 6].map((index) => {
+/**
+ * Gets the shma and tefilla times for the day of the week at the given index (e.g. 0 for Sunday, 1 for Monday, etc.)
+ * @param {number} index
+ * @returns {{day: string, shma: string, tefilla: string}} an object containing the day of the week, shma, and tefilla times for that day
+ */
+function getZmaninForDay(index) {
   const date = sunday.onOrAfter(index); // get the {index}th day of the week
   const zmanim = new Zmanim(newYork, date); // create a zmanim object for the day
   const isToday = date.deltaDays(today) === 0; // is the day of the week today?
@@ -23,6 +28,8 @@ const daysOfTheWeek = [0, 1, 2, 3, 4, 5, 6].map((index) => {
     shma,
     tefilla,
   };
-});
+}
+
+const daysOfTheWeek = [0, 1, 2, 3, 4, 5, 6].map(getZmaninForDay);
 
 console.table(daysOfTheWeek);
